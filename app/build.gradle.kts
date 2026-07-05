4934621fef3df76f4f0b0b531bcfa309e51f423f
qinjunhaocn <qinjunhaocn@users.noreply.github.com>
2026-07-05 14:21:17 +0800

    Fix: use compileSdk 36, disable AAR metadata check (API 37 not available)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.voxyn.looma"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.voxyn.looma"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
